cf5b19e7a9faf513f5e1fb858d04fc262ad8b3db
jfjhh <alex.striff1@gmail.com>
2014-04-06 12:56:12 -0700

    Quotes printing successfully

--- lib/qotd.rb
+++ lib/qotd.rb
@@ -1,5 +1,38 @@
-require "qotd/version"
+require_relative "qotd/version"
 
 module Qotd
-  # Your code goes here...
+
+  def self.quote_file
+    File.read('quotes/quotes.txt')  # => A string of the quotes from the file.
+  end
+
+  def self.quotes
+    quote_file.split(/\n\n/) # => Individual quotes in an array.
+  end
+
+  def self.rand_index
+    rand(quotes.length) # => A random index of quotes array.
+  end
+
+  def self.quote
+    quotes[rand_index] # => A random quote.
+  end
+
+  def self.color
+    "\033[7m"
+  end
+
+  def self.clear
+    "\033[0m"
+  end
+
+  def self.put_quote
+    puts "%s%s%s" % [
+      self.color,
+      self.quote,
+      self.clear
+    ]
+  end
+
 end
+
